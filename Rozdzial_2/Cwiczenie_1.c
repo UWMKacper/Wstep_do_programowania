6eb3cb0537becd3ded3da7612406a8d5a4897da8
Kacper Stanowicki <kacperstanowicki@gmail.com>
2021-10-23 18:26:38 +0200

    Dodanie cwiczenia nr 1 z rozdzialu 2.

---
+++ Rozdzial_2/Cwiczenie_1.c
@@ -0,0 +1,16 @@
+/*Program wypisuje imiê  i nazwisko w ró¿ny sposób.*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    // wypisanie danych w 1 wierszu przy pomocy 1 printf()
+    printf("Kacper Stanowicki\n");
+
+    // wypisanie danych w 2 wierszach przy pomocy 1 printf()
+    printf("Kacper\nStanowicki");
+
+    // wypisanie danych w 1 wierszu przy pomocy 2 printf()
+    printf("\nKacper ");
+    printf("Stanowicki");
+}
