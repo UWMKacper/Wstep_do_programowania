bafa7d56ac57f34036de1a79abc3491af0fd0c4a
Kacper Stanowicki <kacperstanowicki@gmail.com>
2021-10-23 19:21:20 +0200

    Dodanie cwiczenia nr 2 z rozdzialu 2.

---
+++ Rozdzial_2/Cwiczenie_2.c
@@ -0,0 +1,8 @@
+/*Program wypisuje nazwisko oraz adres na ekranie.*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Moje imie to: %s. Moj adres to: %s.", "Kacper", "ul. Warszawska 15a/2");
+}
