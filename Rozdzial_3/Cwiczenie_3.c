f58705b843bbd5380e51d312f4c4eecbdfe6fc5b
Kacper Stanowicki <kacperstanowicki@gmail.com>
2021-11-14 09:17:29 +0100

    Dodanie cwiczenia nr 3 z rozdzialu 3.

---
+++ Rozdzial_3/Cwiczenie_3.c
@@ -0,0 +1,10 @@
+/* Cwiczenie_3.c -- Program wydaje sygnal dzwiekowy i wyswietla tekst. */
+
+#include<stdio.h>
+
+int main(void)
+{
+    printf("\aSally, przerazona niespodziewanym odglosem, krzyknela \"A niech mnie, co to bylo!?\"\n");
+
+    return 0;
+}
