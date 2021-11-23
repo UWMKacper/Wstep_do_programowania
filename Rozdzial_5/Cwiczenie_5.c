bb234d1c6824e9c12b3957a3b64ef9283fe76ccc
Kacper Stanowicki <kacperstanowicki@gmail.com>
2021-11-23 18:46:39 +0100

    Dodanie cwiczenia nr 5 z rozdzialu 5.

---
+++ Rozdzial_5/Cwiczenie_5.c
@@ -0,0 +1,23 @@
+/* Cwiczenie_5.c -- Modyfikacja programu dodaj.c. Teraz probram oblicza
+sume liczb calkowitych dla wartosci podanej przez uzytkownika*/
+/* dodaj.c -- piec typow instrukcji */
+
+#include <stdio.h>
+#include <math.h>
+int main(void)                      /* oblicza sume pierwszych liczb
+calkowitych dla wartosci podanej przez uzytkownika                     */
+{
+  int licznik, suma, granica;       /* instrukcja deklaracji           */
+
+  licznik = 0;                      /* instrukcja przypisania          */
+  suma = 0;                         /* jak wyzej                       */
+
+  printf("Podaj liczbe do ktorej bedzie obliczana suma liczb calkowitych: ");
+  scanf("%d", &granica);
+
+  while (licznik++ < granica)       /* instrukcja                      */
+     suma = suma + pow(licznik,2);  /*       while                     */
+  printf("suma = %d\n", suma);      /* instrukcja wywolania funkcji    */
+
+  return 0;                         /* instrukcja zwrotu */
+}
